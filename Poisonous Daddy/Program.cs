9e314eabdf138c97ce62e238a2d96ddb9c2d5692
NaViSlon <ilyavern@gmail.com>
2016-01-20 19:03:19 +0300

    Update Program.cs

--- Poisonous Daddy/Program.cs
+++ Poisonous Daddy/Program.cs
@@ -97,7 +97,7 @@ namespace Poisonous_Daddy
                             if (t!=null && Utils.SleepCheck("1"))
                             {
                                  v.Hold();
-                                 Utils.Sleep(150, "1");
+                                 Utils.Sleep(120, "1");
                               
 
                         }
